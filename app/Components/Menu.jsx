9f71b716fa77f32b173f1ad28582e2a3f3c8050f
tbrown034 <tbrown034@gmail.com>
2023-05-30 15:37:46 -0500

    WIP feat(header): added headless ui library and using its popover. Also replaced hamburger icon with sytlized menu button

--- app/Components/Menu.jsx
+++ app/Components/Menu.jsx
@@ -1,17 +1,25 @@
 "use client";
-import { Menu } from "@headlessui/react";
+import { Popover } from "@headlessui/react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 export default function Hamburger() {
   return (
     <>
-      <Menu>
-        <Menu.Button>
-          <div className="w-10 sm:w-20">
-            <Bars3Icon />
+      <Popover className="relative">
+        <Popover.Button className="p-2 border-2 border-white rounded-lg hover:bg-white hover:text-sky-800 active:bg-sky-700 active:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
+          Menu
+        </Popover.Button>
+
+        <Popover.Panel className="absolute left-0 right-0 z-10 w-full max-w-full sm:w-64">
+          <div className="flex flex-col rounded-lg bg-sky-400">
+            <Link href="/current">Current Weather</Link>
+            <Link href="/forecast">Tomorrow's Weather</Link>
+            <Link href="/timer">Sunrise and Sunset Timer</Link>
+            <Link href="/about">About</Link>
           </div>
-        </Menu.Button>
-      </Menu>
+        </Popover.Panel>
+      </Popover>
     </>
   );
 }
