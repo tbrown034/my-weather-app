363cdd9c68df1e0864a1f04f47ed77f597b3d989
tbrown034 <tbrown034@gmail.com>
2023-05-30 12:18:09 -0500

    feat: stylized header using flex and tailwind width, also colors and text sizes

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../public/Images/weatherLogo.png";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 export default function Header() {
   return (
     <>
       <header className="flex items-center justify-between px-6 py-4 text-white bg-sky-600">
-        <div className="w-1/4">
-          <Image src={Logo} alt="logo" height={75} />
+        <div className="w-1/12">
+          <Image src={Logo} alt="logo" />
         </div>
-        <Link className="w-1/2 text-xl font-bold text-center" href="/">
+        <Link className="text-5xl font-bold text-center " href="/">
           WeatherApp
         </Link>
-        <div className="w-1/4 text-right">Menu</div>
+        <div className="w-1/12 ">
+          <Bars3Icon />
+        </div>
       </header>
     </>
   );
