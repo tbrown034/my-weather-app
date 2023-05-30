706e8475873d6a169bdb7a0af9d3b19e37c4f91c
tbrown034 <tbrown034@gmail.com>
2023-05-30 11:53:05 -0500

    feat: added basic header with logo, name and menu

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/Images/weatherLogo.png";
+
 export default function Header() {
   return (
     <>
-      <header>
-        <div>Header</div>
+      <header className="flex items-center justify-between px-6 py-4 text-white bg-sky-600">
+        <div className="w-1/4">
+          <Image src={Logo} alt="logo" height={75} />
+        </div>
+        <Link className="w-1/2 text-xl font-bold text-center" href="/">
+          WeatherApp
+        </Link>
+        <div className="w-1/4 text-right">Menu</div>
       </header>
     </>
   );
