4c7169afe95e8db67cc321c3f4de52f8ea7e612c
tbrown034 <tbrown034@gmail.com>
2023-05-31 13:22:50 -0500

    feat: fetched more current weather api data (including converting the dt into readable form) and slightly stylized the displayed data (in yellow)

--- app/current/page.jsx
+++ app/current/page.jsx
@@ -30,15 +30,22 @@ function degToCardinal(deg) {
 
 export default function Current() {
   const [weatherData, setWeatherData] = useState(null);
+  const [updateTime, setUpdateTime] = useState(null); // New state for update time
 
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
         const response = await GET();
         const data = JSON.parse(response.body);
+        const dt = data.dt;
+        const date = new Date(dt * 1000);
+        const localDateStr = date.toLocaleString();
+
+        setUpdateTime(localDateStr);
         setWeatherData(data);
       } catch (error) {
         console.log("Failed to fetch weather data", error);
+        åå;
       }
     };
 
@@ -46,32 +53,55 @@ export default function Current() {
   }, []);
 
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen p-6 scroll bg-sky-800 text-slate-100">
-      <h1 className="text-6xl text-center">OKC&apos;s Current Weather</h1>
+    <section className="flex flex-col items-center justify-center min-h-screen px-6 bg-sky-800 text-slate-100">
+      <h1 className="text-6xl text-center pb-14">OKC&apos;s Current Weather</h1>
       {weatherData && (
-        <div className="flex flex-col gap-20 pt-10 text-2xl">
-          <p>
-            The current temperature is:{" "}
-            <span className="px-2 font-bold underline text-7xl underline-offset-8">
-              {weatherData.main.temp}°F
-            </span>
-          </p>
-          <p>
-            But it actually feels like:{" "}
-            <span className="px-2 font-bold underline text-7xl underline-offset-8">
-              {weatherData.main.feels_like}°F!
-            </span>
-          </p>
-          <p>
-            Our wind speed is:{" "}
-            <span className="px-2 font-bold underline text-7xl underline-offset-8">
-              {weatherData.wind.speed} mph
-            </span>{" "}
-            coming from the{" "}
-            <span className="px-2 font-bold underline text-7xl underline-offset-8">
-              {degToCardinal(weatherData.wind.deg)}!
-            </span>
-          </p>
+        <div className="flex flex-col gap-4 text-xl">
+          <div>
+            <p>
+              The weather right now in{" "}
+              <span className="font-bold text-yellow-400">
+                {weatherData.name}
+              </span>{" "}
+              is{" "}
+              <span className="font-bold text-yellow-400">
+                {weatherData.weather[0].description}
+              </span>
+              .
+            </p>
+          </div>
+          <div>
+            <p>
+              The current temperature is:{" "}
+              <span className="font-bold text-yellow-400">
+                {weatherData.main.temp}°F
+              </span>{" "}
+              but it actually feels like:{" "}
+              <span className="font-bold text-yellow-400">
+                {weatherData.main.feels_like}°F
+              </span>
+              .
+            </p>
+          </div>
+          <div>
+            <p>
+              The wind speed is:{" "}
+              <span className="font-bold text-yellow-400">
+                {weatherData.wind.speed} mph
+              </span>{" "}
+              coming from the{" "}
+              <span className="font-bold text-yellow-400">
+                {degToCardinal(weatherData.wind.deg)}
+              </span>
+              .
+            </p>
+          </div>
+          <div>
+            <p>
+              Weather data last updated:{" "}
+              <span className="font-bold text-yellow-400">{updateTime}</span>.
+            </p>
+          </div>
         </div>
       )}
     </section>
